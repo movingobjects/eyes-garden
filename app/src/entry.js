324f296bbded7e0f8de3613fae275f60898ea1d7
Scott Thiessen <things@varyd.io>
2018-02-04 15:54:26 -0800

    Add visualization structure code

--- app/src/entry.js
+++ app/src/entry.js
@@ -1,2 +1,29 @@
 
+// Imports
+
 import './styles/style.scss';
+
+import Two from 'two.js';
+
+// Constants
+
+// Vars
+
+var elApp = document.getElementById('app');
+var two   = new Two({
+  width: elApp.clientWidth,
+  height: elApp.clientHeight
+}).appendTo(elApp);
+
+
+// Init
+
+init();
+
+function init() {
+
+  two.bind('update', (frameCount) => {
+    console.log(frameCount);
+  }).play();
+
+}
