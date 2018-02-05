68c4b2067b69e0d71b6d9cfb2daaecf08e8fc6c5
Scott Thiessen <things@varyd.io>
2018-02-04 16:46:00 -0800

    Implement svg load in pixi

--- app/src/entry.js
+++ app/src/entry.js
@@ -2,12 +2,24 @@
 // Imports
 
 import './styles/style.scss';
+import * as PIXI from 'pixi.js';
+
+import svgEye from './images/eye.svg';
 
 
 // Constants
 
 
 // Vars
+
+var elApp = document.getElementById('app'),
+    appW  = elApp.clientWidth,
+    appH  = elApp.clientHeight;
+
+var app   = new PIXI.Application(appW, appH),
+    eye   = PIXI.Sprite.fromImage(svgEye);
+
+var timeLastEyeMove = Date.now();
 
 
 // Init
@@ -16,4 +28,29 @@ init();
 
 function init() {
 
+  elApp.appendChild(app.view);
+
+  app.ticker.add(onFrame);
+
+}
+
+// Event handlers
+
+function onFrame(delta) {
+
+  if (Date.now() - timeLastEyeMove > 3000) {
+    moveEye();
+    timeLastEyeMove = Date.now();
+  }
+
+}
+
+
+// Functions
+
+function moveEye() {
+  eye.anchor.set(0.5);
+  eye.x = Math.random() * app.screen.width;
+  eye.y = Math.random() * app.screen.height;
+  app.stage.addChild(eye);
 }
