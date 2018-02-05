6a07d8b6a0699727f040ae526661d3c481e56afa
Scott Thiessen <things@varyd.io>
2018-02-04 17:27:59 -0800

    Add test animatedsprite

--- app/src/entry.js
+++ app/src/entry.js
@@ -6,6 +6,14 @@ import * as PIXI from 'pixi.js';
 
 import svgEye from './images/eye.svg';
 
+import testSeq1 from './images/test-seq-1.png';
+import testSeq2 from './images/test-seq-2.png';
+import testSeq3 from './images/test-seq-3.png';
+import testSeq4 from './images/test-seq-4.png';
+import testSeq5 from './images/test-seq-5.png';
+import testSeq6 from './images/test-seq-6.png';
+import testSeq7 from './images/test-seq-7.png';
+
 
 // Constants
 
@@ -31,6 +39,28 @@ function init() {
   elApp.appendChild(app.view);
 
   app.ticker.add(onFrame);
+
+  spriteTest();
+
+}
+
+function spriteTest() {
+
+  let seq = [
+    testSeq1,
+    testSeq2,
+    testSeq3,
+    testSeq4,
+    testSeq5,
+    testSeq6,
+    testSeq7
+  ].map((seqImg) => PIXI.Texture.fromImage(seqImg));
+
+  let sprite = new PIXI.extras.AnimatedSprite(seq);
+      sprite.animationSpeed = 0.2;
+      sprite.play();
+
+  app.stage.addChild(sprite);
 
 }
 
