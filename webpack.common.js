4af749a5386ab58e3e3274f765f9614b028f96ae
Scott Thiessen <things@sccottt.com>
2018-02-16 16:35:00 -0800

    Move webpack config to root

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, '../app/build'),
+    path: path.resolve(__dirname, './app/build'),
     filename: 'resources/scripts/[name].bundle.js'
   },
 
